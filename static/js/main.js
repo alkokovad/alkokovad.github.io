7cbe9deee4ce6c4f09099d44782bcb091de26331
AlkoKovad <aleksandr.kopeykin@gmail.com>
2021-12-20 23:50:27 +0300

    separated menu parts showing

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,16 @@
+let home = document.querySelector('home')
+let projects = document.querySelector('projects')
+let contact = document.querySelector('contact')
+
+
 const showMenu = (toggleId, navId) =>{
   const toggle = document.getElementById(toggleId);
   nav = document.getElementById(navId);
   if(toggle && nav){
     toggle.addEventListener('click', ()=>{
-      nav.classList.toggle('show')
+       home.classList.toggle('home_show')
+       projects.classList.toggle('projects_show')
+       contact.classList.toggle('show_contact')
     })
   }
 }
