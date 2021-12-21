b60dff330a57d45c8ed278f955ac6c724d18105a
AlkoKovad <aleksandr.kopeykin@gmail.com>
2021-12-21 23:54:24 +0300

    yeah! Looks like I fixed my menu showing

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,19 @@
-let home = document.querySelector('home')
-let projects = document.querySelector('projects')
-let contact = document.querySelector('contact')
+let home = document.getElementById('home')
+let projects = document.getElementById('projects')
+let contact = document.getElementById('contact')
 
 
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId) =>{
   const toggle = document.getElementById(toggleId);
-  nav = document.getElementById(navId);
-  if(toggle && nav){
+  if(toggle){
     toggle.addEventListener('click', ()=>{
-       home.classList.toggle('home_show')
-       projects.classList.toggle('projects_show')
-       contact.classList.toggle('show_contact')
+       home.classList.toggle('hidden');
+       home.classList.toggle('home_show');
+       projects.classList.toggle('hidden');
+       projects.classList.toggle('projects_show');
+       contact.classList.toggle('hidden');
+       contact.classList.toggle('contact_show');
     })
   }
 }
-showMenu('nav-toggle', 'nav-menu')
+showMenu('nav-toggle')
