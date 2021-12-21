f3d5b1a6c7f8d27e543650af16f6dc3245f15dec
AlkoKovad <aleksandr.kopeykin@gmail.com>
2021-12-22 00:40:56 +0300

    I think that's all with menu yet

--- static/js/main.js
+++ static/js/main.js
@@ -7,8 +7,11 @@ const showMenu = (toggleId) =>{
   const toggle = document.getElementById(toggleId);
   if(toggle){
     toggle.addEventListener('click', ()=>{
+       home.classList.toggle('hidden');
        home.classList.toggle('home_show');
+       projects.classList.toggle('hidden');
        projects.classList.toggle('projects_show');
+       contact.classList.toggle('hidden');
        contact.classList.toggle('contact_show');
     })
   }
