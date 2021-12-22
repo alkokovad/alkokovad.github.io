97c04e34c2a92648d3130e11961440f8b5c2e729
AlkoKovad <aleksandr.kopeykin@gmail.com>
2021-12-22 19:40:01 +0300

    Last corrections with positioning before animating

--- static/js/main.js
+++ static/js/main.js
@@ -17,3 +17,15 @@ const showMenu = (toggleId) =>{
   }
 }
 showMenu('nav-toggle')
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+
+  home.classList.toggle('hidden');
+  categories.classList.toggle('hidden');
+  contact.classList.toggle('hidden');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
